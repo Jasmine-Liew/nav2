c7128650b9e877e5147a577755b8df7402b5a5aa
Jasmine-Liew <116135841+Jasmine-Liew@users.noreply.github.com>
2022-10-20 23:34:16 +0800

    Create launchtut.py

---
+++ launchtut.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import IncludeLaunchDescription
+from launch_ros.actions import Node
+TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
+def generate_launch_description():
+ 
+ urdf= os.path.join(get_package_share_directory('turtlebot3_description'), 
+'urdf', 'turtlebot3_burger.urdf')
+ use_sim_time = LaunchConfiguration('use_sim_time', default='True')
+ pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+ 
+ world_file_name = 'empty_worlds/' + TURTLEBOT3_MODEL + '.model'
+ world = os.path.join(get_package_share_directory('turtlebot3_gazebo'),
+ 'worlds', world_file_name)
+ launch_file_dir = os.path.join(get_package_share_directory('gazebo_ros'), 
+'launch')
+ 
+ 
+ 
+ 
+ return LaunchDescription([
+ IncludeLaunchDescription(
+ PythonLaunchDescriptionSource(
+ os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')
+ ),
+ launch_arguments={'world': world}.items(),
+ ),
+ IncludeLaunchDescription(
+ PythonLaunchDescriptionSource(
+ os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')
+ ),
+ ),
+ 
+ 
+ 
+ Node(
+ package='robot_state_publisher',
+ executable='robot_state_publisher',
+ name='robot_state_publisher',
+ output='screen',
+ parameters=[{'use_sim_time': use_sim_time}],
+ arguments=[urdf]),
+ 
+ 
+ ])
